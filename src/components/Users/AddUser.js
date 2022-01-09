19e8517aa6e89b26a0183940ae5fabc2a87bb9fe
Yashar Akish <yashar_akish@yahoo.com>
2022-01-08 20:39:18 -0500

    adding helper to wrapping the jsx elements

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 import classes from "./AddUser.module.css";
 
@@ -49,7 +50,7 @@ const AddUser = (props) => {
     setError(null);
   }
   return (
-    <div >
+    <Wrapper >
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input} >
         <form onSubmit={addUserHandler} >
@@ -60,7 +61,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
 
   );
 };
